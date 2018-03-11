17b29ac8258c488bf807ad33e5eabaf55f0d7418
Davanon <davanon2014@gmail.com>
2018-03-11 11:56:42 +0530

    First

--- src/main/java/com/iqmsoft/SimpleController.java
+++ src/main/java/com/iqmsoft/SimpleController.java
@@ -1,6 +1,8 @@
 package com.iqmsoft;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,19 +15,36 @@ public class SimpleController {
 
 	private static final Logger logger = LoggerFactory.getLogger(SimpleController.class);
 	
+	private AtomicInteger a = new AtomicInteger(0);
+	
+	private ConcurrentHashMap<String, String> c = new ConcurrentHashMap<>();
+	
 	@RequestMapping("/start")
 	public void start() throws InterruptedException {
+		a.getAndIncrement();
+        c.put("one", "oneall");	
 		log();
+		
 	}
 	
 	@RequestMapping("/startTask")
 	public void start(@RequestParam(value="name") String task) throws InterruptedException {
+		
+		a.getAndIncrement();
+		c.put("two", "twoall");	
 		log();
+		
 	}
 	
-	private void log() throws InterruptedException {
+	private synchronized void log() throws InterruptedException {
+		
+		logger.info("Concurrent: {}", c.toString());
+		
 		logger.info("Start: {}", Thread.currentThread().getName());
 		TimeUnit.SECONDS.sleep(10);
 		logger.info("Stop: {}", Thread.currentThread().getName());
+		
+		logger.info("Atomic: {}", a.get());
+		
 	}
 }
